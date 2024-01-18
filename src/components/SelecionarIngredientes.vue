f623215afadb5013f1c00cbd537e9cc1227ce35b
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-18 14:11:31 -0300

    Adicionando imagens dinamicamente

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index'
 import type ICategoria from '@/interfaces/ICategoria'
+import CardCategoria from './CardCategoria.vue'
 export default {
   data() {
     return {
@@ -9,7 +10,8 @@ export default {
   },
   async created() {
     this.categorias = await obterCategorias()
-  }
+  },
+  components: { CardCategoria }
 }
 </script>
 
@@ -23,7 +25,7 @@ export default {
 
     <ul class="categorias">
       <li v-for="categoria in categorias" :key="categoria.nome">
-        <p>{{ categoria.nome }}</p>
+        <CardCategoria :categoria="categoria" />
       </li>
     </ul>
     <p class="paragrafo dica">*Atenção consideramos que você tem em casa sal, pimenta e agua</p>
